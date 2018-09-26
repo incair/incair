cd98d9fe3c3c823b7f8ca974a54a9bde66c8a2c5
RevelMind <36819949+RevelMind@users.noreply.github.com>
2018-09-25 16:15:05 -0800

    Auth file

---
+++ application/config/data/auth.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+	// Default Authentication Driver
+	*/
+	'driver' => 'eloquent',
+
+	// Authentication Username
+	'username' => 'email',
+	
+	// Authentication Password
+	'password' => 'password',
+	
+	// Authentication Model
+	'model' => 'User',
+	
+	// Authentication Table
+	'table' => 'users',
+);
